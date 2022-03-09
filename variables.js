5fb5920fab3513d41c15991d98792e1f37ca9200
Gabriel Cardoso Arruda <gabriel.garruda@tivit.com>
2022-03-09 15:47:47 -0300

    Learning Javascript

--- variables.js
+++ variables.js
@@ -8,5 +8,14 @@ boolean: true or false
 string: text
 symbol: primitive value that's unique
 number: numbers
-object: can store a lot a different 
+object: can store a lot a different pairs
+
 */
+
+var myName = "Gabriel" //var it's use be throughout whole program
+
+myName = 8;
+
+let ourName = "freeCodeCamp" // let will only be used within the scope of where you declared that.
+
+const pi = 3.14; //const is a variable that should never change
