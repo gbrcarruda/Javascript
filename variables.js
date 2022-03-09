e4d4982c8e0befaf0932be17b26376078933f050
Gabriel Cardoso Arruda <gabriel.garruda@tivit.com>
2022-03-09 07:01:52 -0300

    Javascript Course

---
+++ variables.js
@@ -0,0 +1,12 @@
+// in-line comment
+/*multiple line comment*/
+
+/*Data Types in Javascript:
+undefined: a variable that you haven't to set anything yet
+null: it's nothing
+boolean: true or false
+string: text
+symbol: primitive value that's unique
+number: numbers
+object: can store a lot a different 
+*/
